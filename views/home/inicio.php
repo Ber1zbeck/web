48a7ad09a1183e752759fea5e9ba912b0de93f65
Berizbeck <alexandervelazuez@gmail.com>
2026-03-09 18:28:49 +0000

    9 mar 2026, 12:28

--- views/home/inicio.php
+++ views/home/inicio.php
@@ -2,21 +2,9 @@
 
 
 <body>
-    <header>
-        <div class="logo"><span>SIR</span>
-            <div class="logo-icon">
-                <svg viewBox="0 0 24 24">
-                    <path
-                        d="M4 19h16v2H4zM20 3H4v10c0 2.21 1.79 4 4 4h6c2.21 0 4-1.79 4-4v-3h2c1.11 0 2-.89 2-2V5c0-1.11-.89-2-2-2zm-4 10c0 1.1-.9 2-2 2H8c-1.1 0-2-.9-2-2V5h10v8zm4-5h-2V5h2v3z" />
-                </svg>
-            </div>
-        </div>
-        <nav>
-            <a href="#">Inicio</a>
-            <a href='?page=login'>Iniciar sesión</a>
-            <a href='?page=registro' class="btn-nav">Empezar</a>
-        </nav>
-    </header>
+    
+<?php include $templates_navbar; ?>
+
 
     <section class="hero">
         <h1>SIR.<br><span>Sistema Inteligente de Reciclaje</span></h1>
