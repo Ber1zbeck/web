928bae1f47ac9b151b87afbb5c020d23ea7aeb74
Alexander Rosales <alexandervelazuez@gmail.com>
2026-03-23 01:24:44 +0000

    22 mar 2026, 19:24

--- directorios.php
+++ directorios.php
@@ -20,7 +20,16 @@ $templates_navbar = $base_dir . "/template/navbar.php";
 $templates_footer = $base_dir . "/template/footer.php";
 
 #Usuario
-
 $templates_navbar_usu = $base_dir . "/template/navbar_usuario.php";
 $templates_footer_usu = $base_dir . "/template/footer_usuario.php";
 $templates_header_usu = $base_dir . "/template/header_usuario.php";
+
+#Administrador
+$templates_navbar_admin = $base_dir . "/template/navbar_admin.php";
+$templates_footer_admin = $base_dir . "/template/footer_admin.php";
+$templates_header_admin = $base_dir . "/template/header_admin.php";
+
+#Moderador
+$templates_navbar_mod = $base_dir . "/template/navbar_moderador.php";
+$templates_footer_mod = $base_dir . "/template/footer_moderador.php";
+$templates_header_mod = $base_dir . "/template/header_moderador.php";
